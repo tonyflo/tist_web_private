ab3b36a7f2a97a7497a66eb686451bd97f5a496c
Nick Mathews <nickinnik@gmail.com>
2015-05-19 01:17:38 -0700

    welcome_email,php is the html. Still needs to have the dynamic stuff integrated.

---
+++ welcome_email.php
@@ -0,0 +1,123 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Welcome to The Tist</title>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style id="style-1-cropbar-clipper">
+.en-markup-crop-options {
+    top: 18px ;
+    left: 50% ;
+    margin-left: -100px;
+    width: 200px;
+    border: 2px rgba(255,255,255,.38) solid ;
+    border-radius: 4px ;
+}
+
+.en-markup-crop-options div div:first-of-type {
+    margin-left: 0px ;
+}
+</style></head>
+<body style="margin: 0px; padding: 0px;" class=" hasGoogleVoiceExt">
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">	
+		<tbody><tr>
+			<td style="padding: 10px 0 30px 0;">
+				<table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #cccccc; border-collapse: collapse;">
+					<tbody><tr>
+						<td align="center" bgcolor="#8a8" style="padding: 40px 0 30px 0; color: #153643; font-size: 28px; font-weight: bold; font-family: Arial, sans-serif;">
+							<img src="img/The_Tist_Logo_1.png" alt="The Tist Logo" width="230" height="230" style="display: block;">
+						</td>
+					</tr>
+					<tr>
+						<td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+							<table border="0" cellpadding="0" cellspacing="0" width="100%">
+								<tbody><tr>
+									<td style="color: #153643; font-family: Arial, sans-serif; font-size: 24px;">
+										<b>Welcome to The Tist!</b>
+									</td>
+								</tr>
+								<tr>
+									<td style="padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+										Nick, you are now part of a growing community of scientists communicating with the public about their research.  
+										This website allows the general public an opportunity to hear about science news and methods from the scientists themselves, by providing all of the tools necessary for scientists to communicate their research clearly through the internet.
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<table border="0" cellpadding="0" cellspacing="0" width="100%">
+											<tbody><tr>
+												<td width="260" valign="top">
+													<table border="0" cellpadding="0" cellspacing="0" width="100%">
+														<tbody><tr>
+															<td>
+																<img src="img/rocket.png" alt="" width="100%" height="180" style="display: block;">
+															</td>
+														</tr>
+														<tr>
+															<td style="padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px; display: block;">
+															Congratulations!  Your account is almost verified.  
+															You simply need to verify your Kutztown University of Pennsylvania email address here. 
+															Your Activation Code is <a href="http://the-tist.com/confirmation.php?activation_code=5556b30029135">5556b30029135</a>
+															</td>
+														</tr>
+													</tbody></table>
+												</td>
+												<td style="font-size: 0; line-height: 0;" width="20">
+													&nbsp;
+												</td>
+												<td width="260" valign="top">
+													<table border="0" cellpadding="0" cellspacing="0" width="100%">
+														<tbody><tr>
+															<td>
+																<img src="img/profile.png" alt="" width="100%" height="180" style="display: block;">
+															</td>
+														</tr>
+														<tr>
+															<td style="padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+															Next, you can create a profile page where other colleagues, friends, family and fans can find you.  Go ahead! Start building on your Tist profile!
+															</td>
+														</tr>
+													</tbody></table>
+												</td>
+											</tr>
+										</tbody></table>
+									</td>
+								</tr>
+							</tbody></table>
+						</td>
+					</tr>
+					<tr>
+						<td bgcolor="#8a8" style="padding: 30px 30px 30px 30px;">
+							<table border="0" cellpadding="0" cellspacing="0" width="100%">
+								<tbody><tr>
+									<td style="color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;" width="75%">
+										&copy Science Share, LLC, 2015<br>
+									</td>
+									<td align="right" width="25%">
+										<table border="0" cellpadding="0" cellspacing="0">
+											<tbody><tr>
+												<td style="font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;">
+													<a href="https://plus.google.com/+ScienceShareReading/posts" style="color: #ffffff;">
+														<img src="img/googleplus.png" alt="Google Plus" width="38" height="38" style="display: block;" border="0">
+													</a>
+												</td>
+												<td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+												<td style="font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;">
+													<a href="https://www.facebook.com/itsallinthetist" style="color: #ffffff;">
+														<img src="img/facebook.png" alt="Facebook" width="38" height="38" style="display: block;" border="0">
+													</a>
+												</td>
+											</tr>
+										</tbody></table>
+									</td>
+								</tr>
+							</tbody></table>
+						</td>
+					</tr>
+				</tbody></table>
+			</td>
+		</tr>
+	</tbody></table>
+</body></html>
+
